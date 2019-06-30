df9a250fe1a4b5ce2638b59c7c5dc003e48ddd44
Ajay-Argade <ajayargade1996@gmail.com>
2019-06-30 19:38:20 +0530

    task 1 save login name

--- app/src/main/java/co/uttar/testproject/ActivityHome.java
+++ app/src/main/java/co/uttar/testproject/ActivityHome.java
@@ -1,13 +1,51 @@
 package co.uttar.testproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class ActivityHome extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                moveTaskToBack(true);
+                break;
+        }
+        return true;
+    }
+
+    public void logOut(View view) {
+
+        sharedPreferences = getSharedPreferences(MainActivity.SHARED_PREF_NAME, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.remove("password");
+
+        editor.commit();
+        Log.d("success", "logged out");
+        Intent goToHomeActivity = new Intent(this.getApplicationContext(), MainActivity.class);
+
+        startActivity(goToHomeActivity);
+    }
+
 }
+
+
--- app/src/main/java/co/uttar/testproject/MainActivity.java
+++ app/src/main/java/co/uttar/testproject/MainActivity.java
@@ -1,52 +1,68 @@
 package co.uttar.testproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import static android.R.attr.name;
-
 public class MainActivity extends AppCompatActivity {
 
+    private Button b1;
+    private EditText email, pwd, name;
+    public static String SHARED_PREF_NAME = "mySharedPreference";
 
-        private Button b1;
-        private EditText email, pwd, name ;
+  SharedPreferences sharedPreferences;
+   SharedPreferences.Editor editor;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1 = (Button)  findViewById(R.id.button);
-        email= (EditText) findViewById(R.id.email);
+        b1 = (Button) findViewById(R.id.button);
+        email = (EditText) findViewById(R.id.email);
         pwd = (EditText) findViewById(R.id.pwd);
-
         name = (EditText) findViewById(R.id.name);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        name.setText(sharedPreferences.getString("username", null));
+
+        pwd.setText(sharedPreferences.getString("password", null));
+
 
 
 
     }
-    public void Login(View view){
-        if( name.getText().toString().equals("ajay") &&
-                pwd.getText().toString().equals("ajay"))
-        {
-            Log.d("success", "logged in");
-            Intent gotohomeActivity = new Intent(this.getApplicationContext(),ActivityHome.class);
 
-            startActivity(gotohomeActivity);
+
+    public void Login(View view) {
+        String usrname = name.getText().toString();
+        String passwrd = pwd.getText().toString();
+        if (!name.getText().toString().equals("ajay") &&
+                !pwd.getText().toString().equals("ajay")) {
+
+            Log.d("success", "logged in");
+            Intent goToHomeActivity = new Intent(this.getApplicationContext(), ActivityHome.class);
+            startActivity(goToHomeActivity);
 
             Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-            Log.d("fail", "not logg in");
+        } else {
+            Log.d("fail", "not log in");
             Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_LONG).show();
+
         }
+
+        editor.putString("username", usrname);
+        editor.putString("password", passwrd);
+        editor.commit();
+
 
     }
 }
